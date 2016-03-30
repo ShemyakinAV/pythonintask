a784a54f0194a28d441e7940ad33790aa93e7278
Shemyakin_A_V <antonshemyakin97@gmail.com>
2016-03-30 20:00:39 +0300

    Шемякин А.В. Задача 5, вариант 31. Выполнил задачу

---
+++ INBa/2015/SHEMYAKIN_A_V/task_5_31.py
@@ -0,0 +1,5 @@
+creators = ["Кирилл", "Мефодий"] #список создателей
+import random as random_number; #импорт оператора random
+print ("Данная программа выводит имя одного из создателей старославянской азбуки в случайном порядке");
+print (random_number.choice(creators)); #случайный выбор из списка
+print ("Press Enter to exit");
